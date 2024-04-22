345c5910b79ef99b14caae44a48f9bac7d841de9
Ariel_Santangelo <78393362+ariel3259@users.noreply.github.com>
2024-04-22 16:52:12 -0300

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,6 @@ load_dotenv(find_dotenv(), override=True)
 def create_app():
     app = Flask(__name__)
     db_url = os.getenv("DB_URL")
-    app.config["ENBINE"] = create_engine(db_url, echo=True)
+    app.config["ENGINE"] = create_engine(db_url, echo=True)
     
     return app
